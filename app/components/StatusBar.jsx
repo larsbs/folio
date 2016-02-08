bff57418f57a1f8fc0cb698c89e6f096d8f28dcf
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-08 19:00:04 +0100

    Now status bar counts lines and words

--- app/components/StatusBar.jsx
+++ app/components/StatusBar.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import style from 'less/components/status-bar';
 
 
-const StatusBar = () => (
-  <div className={style.statusBar + ' status-bar'}>
-    <div className="left-status-bar">
-      <span>Lines: 4, Words: 20, Characters: 241</span>
-    </div>
-    <div className="right-status-bar">
-    </div>
-  </div>
-);
+class StatusBar extends React.Component {
+
+  render () {
+    console.log();
+    const lines = this.props.codeText.split(/\r\n|\r|\n/).length;
+    const words = this.props.codeText.replace(/[^A-Z\d\s]/gi, '').split(/\s+/).filter(x => !!x).length;
+    return (
+      <div className={style.statusBar + ' status-bar'}>
+        <div className="left-status-bar">
+          <span>Lines: {lines}, Words: {words}</span>
+        </div>
+        <div className="right-status-bar">
+        </div>
+      </div>
+    );
+  }
+
+}
 
 
-export default StatusBar;
+const mapStateToProps = (state) => {
+  return {
+    codeText: state.code.text
+  };
+};
+
+
+export default connect(mapStateToProps)(StatusBar);
