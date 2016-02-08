a6f4dc4267a3ffb97f8757eed1f501bcd71d2c9f
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-08 18:33:59 +0100

    Added hover effect to menu bar items

--- app/components/MenuBar.jsx
+++ app/components/MenuBar.jsx
@@ -6,10 +6,10 @@ const MenuBar = () => (
   <div className={styles.menuBar + ' menu-bar'}>
     <i className="fa fa-fw fa-bold" />
     <i className="fa fa-fw fa-italic" />
-    <i className="fa fa-fw" />
+    <i className="fa fa-fw separator" />
     <i className="fa fa-fw fa-link" />
     <i className="fa fa-fw fa-photo" />
-    <i className="fa fa-fw" />
+    <i className="fa fa-fw separator" />
     <i className="fa fa-fw fa-indent" />
     <i className="fa fa-fw fa-list-ul" />
     <i className="fa fa-fw fa-list-ol" />
