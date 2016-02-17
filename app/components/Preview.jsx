0bf80021b39e177494247f120b16717c47bc81f8
Lorenzo Ruiz <lars.bs@hotmail.com>
2016-02-17 17:31:56 +0100

    Added Github theme for the markdown

--- app/components/Preview.jsx
+++ app/components/Preview.jsx
@@ -3,6 +3,8 @@ import marked from 'marked';
 import Highlight from 'highlight.js';
 
 import 'highlight.js/styles/github.css';
+import 'github-markdown-css/github-markdown.css';
+import 'less/components/preview.less';
 
 
 class Preview extends React.Component {
@@ -10,7 +12,7 @@ class Preview extends React.Component {
   render() {
     return (
       <div className="preview-container">
-        <div className="preview" dangerouslySetInnerHTML={this._rawMarkup()} />
+        <div className="preview markdown-body" dangerouslySetInnerHTML={this._rawMarkup()} />
       </div>
     );
   }
